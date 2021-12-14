664c49141a8f9a2bf16f57a711b54cbe0751d313
mohitrathor98 <mohitrathor8@gmail.com>
2021-12-14 12:09:51 +0530

    starter code ready

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("Indian State Game")
+
+image = "india-blank-map.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+turtle.resizemode("user")
+
+
+turtle.mainloop()
