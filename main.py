42d27dedc0e6df319b4fa97a189fbc11a37dbaf5
mohitrathor98 <mohitrathor8@gmail.com>
2021-12-14 13:25:07 +0530

    starter code ready

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import turtle
-import pandas
+import pandas as pd
+
+from state import State
 
 screen = turtle.Screen()
 screen.title("Indian State Game")
@@ -10,5 +12,14 @@ screen.addshape(image)
 turtle.shape(image)
 turtle.resizemode("user")
 
+data = pd.read_csv('state.csv')
+for _ in len(data['state']):
+    print(turtle.textinput("What's another state name?", "prompt"))
+# placing all state names at their respective positions
+
+state_ob = State()
+for index, row in data.iterrows():
+    state_ob.write_state(row['state'], row['X'], row['Y'])
+    #print(row['state'], row['X'], row['Y'])
 
 turtle.mainloop()
