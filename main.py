5a5925937900e1a2ae9e620bb47b8bdc74b7eca6
mohitrathor98 <mohitrathor8@gmail.com>
2021-12-15 10:44:27 +0530

    feature add

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 from state import State
 
 screen = turtle.Screen()
+screen.setup(width=1000, height=1000)
+
 screen.title("Indian State Game")
 
 image = "india-blank-map.gif"
@@ -16,11 +18,13 @@ state_ob = State()
 data = pd.read_csv('state.csv')
 data_dict = {'state': [state.lower() for state in data['state'].tolist()], 'x': data['X'].tolist(), 'Y': data['Y'].tolist()}
 
-for _ in range(len(data['state'])):
-    state_name = turtle.textinput("What's another state name?", "prompt")
+correctly_guessed = []
+while len(correctly_guessed) < len(data_dict['state']):
+    state_name = turtle.textinput(f"{len(correctly_guessed)}/{len(data_dict['state'])} states correct", "State Name:").lower()
     
-    if state_name.lower() in data_dict['state']:
-        index = data_dict['state'].index(state_name.lower())
+    if (state_name in data_dict['state']) and (state_name not in correctly_guessed):
+        correctly_guessed.append(state_name)
+        index = data_dict['state'].index(state_name)
         state_ob.write_state(state_name.title(), data_dict['x'][index], data_dict['Y'][index])
 
 turtle.mainloop()
