defc875d4050a40f93a10d41b27f0a1f45136250
mohitrathor98 <mohitrathor8@gmail.com>
2021-12-15 11:00:05 +0530

    feature add

--- main.py
+++ main.py
@@ -16,15 +16,22 @@ turtle.resizemode("user")
 state_ob = State()
 
 data = pd.read_csv('state.csv')
-data_dict = {'state': [state.lower() for state in data['state'].tolist()], 'x': data['X'].tolist(), 'Y': data['Y'].tolist()}
+data_dict = {'state': data['state'].tolist(), 'x': data['X'].tolist(), 'Y': data['Y'].tolist()}
 
 correctly_guessed = []
 while len(correctly_guessed) < len(data_dict['state']):
-    state_name = turtle.textinput(f"{len(correctly_guessed)}/{len(data_dict['state'])} states correct", "State Name:").lower()
+    state_name = turtle.textinput(f"{len(correctly_guessed)}/{len(data_dict['state'])} states correct", "State Name:").title()
+    
+    if state_name == "Exit":
+        break
     
     if (state_name in data_dict['state']) and (state_name not in correctly_guessed):
         correctly_guessed.append(state_name)
         index = data_dict['state'].index(state_name)
-        state_ob.write_state(state_name.title(), data_dict['x'][index], data_dict['Y'][index])
+        state_ob.write_state(state_name, data_dict['x'][index], data_dict['Y'][index])
 
-turtle.mainloop()
+# dumping missed state into a csv
+missed_state = list(set(data_dict['state']) - set(correctly_guessed))
+if len(missed_state) > 0:
+    data = pd.DataFrame({'Missed States': missed_state})
+    data.to_csv("states_to_learn.csv")
